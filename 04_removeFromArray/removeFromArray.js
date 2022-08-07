b47d1efe5b15b20dee7dac02172104394b4685f6
JVPH <gyrospizzeria@protonmail.com>
2022-08-06 22:26:18 -0500

    Implement better solution.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,32 +1,15 @@
-const removeFromArray = function (array, ...toBeRemoved) {
-    toBeRemoved = Array.from(toBeRemoved);
-    let count = 0;
-    for(let j = 0; j<toBeRemoved.length; j++){
-        if (!(array.includes(toBeRemoved[j]))) {
-            count++;
-        }
-    }
-    if(count=0){
-        return array;
-    }
-    
-    if(arguments.length>2){
-        for (let i = array.length - 1; i >= 0; i--) {
-            for(let j = 0; j<toBeRemoved.length; j++){
-                if (array[i] === toBeRemoved[j]) {           
-                    array.splice(i, 1);
-                }
-            }
+const removeFromArray = function (...args) {
+    const array = arguments[0];
+    const newArray = [];
+    array.forEach((element) => {
+        if(!args.includes(element)){
+            newArray.push(element);
         }
 
-    }
+    })
 
-    for (let i = array.length - 1; i >= 0; i--) {
-        if (array[i] === toBeRemoved[0]) {           
-            array.splice(i, 1);
-        }
-    }
-    return array;
+    return newArray;
+
 };
 
 // Do not edit below this line
