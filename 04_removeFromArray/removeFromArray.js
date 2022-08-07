b648b8d5d8728f0d8846b5beeff8a0a2b5d049c4
JVPH <gyrospizzeria@protonmail.com>
2022-08-06 19:45:50 -0500

    Add solution when only one element is going to be removed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...toBeRemoved) {
+    if (!(array.includes(arguments[1]))) {
+        console.log('hi');
+        return array;
+    }
 
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (array[i] === arguments[1]) {
+            console.log('phio');
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
