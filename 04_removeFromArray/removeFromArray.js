b3ac5d3221edf0f9e81e4f1a8e8659c418f4618f
JVPH <gyrospizzeria@protonmail.com>
2022-08-06 22:09:16 -0500

    Add working solution for all tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,28 @@
 const removeFromArray = function (array, ...toBeRemoved) {
-    if (!(array.includes(arguments[1]))) {
-        console.log('hi');
+    toBeRemoved = Array.from(toBeRemoved);
+    let count = 0;
+    for(let j = 0; j<toBeRemoved.length; j++){
+        if (!(array.includes(toBeRemoved[j]))) {
+            count++;
+        }
+    }
+    if(count=0){
         return array;
+    }
+    
+    if(arguments.length>2){
+        for (let i = array.length - 1; i >= 0; i--) {
+            for(let j = 0; j<toBeRemoved.length; j++){
+                if (array[i] === toBeRemoved[j]) {           
+                    array.splice(i, 1);
+                }
+            }
+        }
+
     }
 
     for (let i = array.length - 1; i >= 0; i--) {
-        if (array[i] === arguments[1]) {
-            console.log('phio');
+        if (array[i] === toBeRemoved[0]) {           
             array.splice(i, 1);
         }
     }
