36c3f7871d22d78c6aff89cb8de8ae4a0a4ead3f
JVPH <gyrospizzeria@protonmail.com>
2022-08-08 01:28:18 -0500

    Solution for all cases.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function (fNumber, lNumber) {
+    if (typeof (fNumber) === "number" && typeof (lNumber) === "number" && fNumber>=0 && lNumber>=0) {
+        let upperLimit = lNumber;
+        let lowerLimit = fNumber;
+        if(fNumber>lNumber){
+            upperLimit = fNumber;
+            lowerLimit = lNumber;
+        }
+        
+        let sum = 0;
+        for (let i = lowerLimit; i <= upperLimit; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    return "ERROR";
 
 };
 
