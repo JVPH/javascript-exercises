96136e9f34b7f6e7dc176ccb4c2825b84a8b9212
JVPH <gyrospizzeria@protonmail.com>
2022-08-17 19:42:26 -0500

    Add solution for all test cases of tempConversion exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const ftoc = function() {
+const ftoc = function(tempC) {
+    const conversion = ((tempC - 32)*(5/9));
+    return  Math.round(conversion * 10) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(tempF) {
+  const conversion = (tempF*(9/5)) + 32;
+  return  Math.round(conversion * 10) / 10;
 
 };
 
