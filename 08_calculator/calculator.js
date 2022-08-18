fc58452bb27c93830038420fedd07e0f11ec77e4
JVPH <gyrospizzeria@protonmail.com>
2022-08-17 21:55:14 -0500

    Add solution for all test cases of calculator exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
+const add = function(a, b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a-b;	
+};
+
+const sum = function(array) {
+  return array.reduce((total, arg) => total + arg, 0);
+};
+
+const multiply = function(array) {
+  return array.reduce((total, arg) => total * arg);  
+
+};
+
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);  
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  if(num === 0){
+    return 1;
+  }
+  let factorial = num;
+  for(let i = num; i > 1; i--){    
+    factorial = factorial*(i-1);
+  }
+  return factorial;
 	
 };
 
