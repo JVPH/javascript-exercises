7c716db4ed0539f9df788e6f1c327fc1944f41d5
JVPH <gyrospizzeria@protonmail.com>
2022-08-29 18:16:18 -0500

    add solution of all test cases of findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = 2022;
+    
+    people.forEach(person => {
+        if(person.yearOfDeath === undefined){      
+            person.age = currentYear - person.yearOfBirth;
+        }else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
 
+    let oldest = {name: 'placeholder', yearOfBirth: 0, yearOfDeath: 0, age: 0};  
+
+    people.forEach((person) => {
+        if (person.age > oldest.age) {            
+            oldest = person;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
