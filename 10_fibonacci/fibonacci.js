1862fa599b27b85b56e29ab67a253352379656f8
JVPH <gyrospizzeria@protonmail.com>
2022-08-29 13:47:49 -0500

    add solution for all test cases of fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number<0){
+        return 'OOPS';
+    }
+    const fNumber = parseInt(number);
+    const phi = (1 + Math.sqrt(5)) / 2;
+    const result = (Math.pow(phi,fNumber) - Math.pow((1-phi), fNumber)) / Math.sqrt(5);
+    return Math.round(result);
 };
 
 // Do not edit below this line
