54e304b46ed9595922b2fd4bc9b1787fe92fbb65
JVPH <gyrospizzeria@protonmail.com>
2022-08-29 11:59:00 -0500

    add solution to all test cases of the palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const onlyLetters = Array.from(str.toLowerCase().replace(/[^a-z]/gi, ""));
+    const reversed = [...onlyLetters].reverse();
+    for (let i = 0; i < onlyLetters.length; i++) {
+        if(onlyLetters[i] !== reversed[i]){
+            return false;
+        }        
+    }
+    return true;
 };
 
 // Do not edit below this line
